d297f82e214c0b8228aa42e4abc4698fe68f9aa7
Christian <christiancharlton17@gmail.com>
2020-11-15 15:32:51 -0700

    Added CRUD functionality using existing REST API and frontend using Vue 2. Added requirements.txt for ease of use on other machines.

---
+++ vue-js-client-crud/src/http-common.js
@@ -0,0 +1,11 @@
+
+// initializes axios with HTTP base Url and headers
+
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
---
+++ vue-js-client-crud/src/router.js
@@ -0,0 +1,29 @@
+
+// defines routes for each component
+
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/tutorials",
+      name: "tutorials",
+      component: () => import("./components/TutorialsList")
+    },
+    {
+      path: "/tutorials/:id",
+      name: "tutorial-details",
+      component: () => import("./components/Tutorial")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/AddTutorial")
+    }
+  ]
+});
